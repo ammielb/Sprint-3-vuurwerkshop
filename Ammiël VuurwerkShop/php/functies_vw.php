9ccd7488034a423377ac03b25d0964c205c70da8
ammielb <58260687+ammielb@users.noreply.github.com>
2019-12-18 13:18:41 +0100

    Add files via upload

--- Ammiël VuurwerkShop/php/functies_vw.php
+++ Ammiël VuurwerkShop/php/functies_vw.php
@@ -4,7 +4,7 @@
   <?php
 
 /* product printen voor sier vw*/
-  function sier(){
+  function cat($welk,$cat){
     $servername = "localhost"; 
     $username = "root";
     $password = "";
@@ -12,67 +12,46 @@
     
     $conn = new PDO("mysql:host=$servername; dbname=$db_name;", $username, $password,);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $siervw = "SELECT * FROM vuurwerk WHERE `categorie` = 'sier' ";
-    $result = $conn -> query($siervw);
-    echo"<div class='container' align='center'>";
-    while($row = $result -> fetch()){
-        echo"<div class='product'><br><br>";
-        echo"<img height=200 src='$row[img]'>";
-        echo $row['naam'] . "</img><br>";
-        echo"artikel_ID:" . $row['art_id'] . "<br>";
-        echo $row['euro']."<br>";
+
+if($welk == 'ander'){
+        $sql = "SELECT * FROM vuurwerk WHERE `categorie` = '$cat' ";
+        $result = $conn -> query($sql);
+
+        echo"<div class='container' align='center'>";
+        while($row = $result -> fetch()){
+                 echo"<div class='product'><br><br>";
+                 echo"<img height=200 src='$row[img]'>";
+                 echo $row['naam'] . "</img><br>";
+                 echo"artikel_ID:" . $row['art_id'] . "<br>";
+                 echo $row['euro']."<br>";
+                 echo"<button class='toevoegbutton'>Toevoegen</button>";
         
         echo"</div>";
 
     }
     echo"</div>";
-}  
-/* product printen voor knal vw*/
-function knal(){
-    $servername = "localhost"; 
-    $username = "root";
-    $password = "";
-    $db_name="vuurwerk";
+}elseif($welk =='alle'){
+    $sql = "SELECT * FROM vuurwerk";
+    $result = $conn -> query($sql);
+
+    echo"<div class='container' align='center'>";
+    while($row = $result -> fetch()){
+             echo"<div class='product'><br><br>";
+             echo"<img height=200 src='$row[img]'>";
+             echo $row['naam'] . "</img><br>";
+             echo"artikel_ID:" . $row['art_id'] . "<br>";
+             echo $row['euro']."<br>";
+             echo"<button class='toevoegbutton'>Toevoegen</button>";
     
-        $conn = new PDO("mysql:host=$servername; dbname=$db_name;", $username, $password,);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $knalvw = "SELECT * FROM vuurwerk WHERE `categorie` = 'knal' ";
-        $result = $conn -> query($knalvw);
-        echo"<div class='container' align='center'>";
-        while($row = $result -> fetch()){
-            echo"<div class='product'><br><br>";
-            echo"<img height=200 src='$row[img]'>";
-            echo $row['naam'] . "</img><br>";
-            echo"artikel_ID:" . $row['art_id'] . "<br>";
-            echo $row['euro']."<br>";
-            echo"</div>";
-    
-        }
-        echo"</div>";
-    }  
-/* product printen voor knal vw*/
-function alle(){
-    $servername = "localhost"; 
-    $username = "root";
-    $password = "";
-    $db_name="vuurwerk";
-    
-        $conn = new PDO("mysql:host=$servername; dbname=$db_name;", $username, $password,);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $vw_alles = "SELECT * FROM vuurwerk";
-        $result = $conn -> query($vw_alles);
-        echo"<div class='container' align='center'>";
-        while($row = $result -> fetch()){
-            echo"<div class='product'><br><br>";
-            echo"<img height=200 src='$row[img]'>";
-            echo $row['naam'] . "</img><br>";
-            echo"artikel_ID:" . $row['art_id'] . "<br>";
-            echo $row['euro']."<br>";
-            echo"</div>";
-    
-        }
     echo"</div>";
-    }
+
+}
+echo"</div>";
+}
+
+
+
+}  
 
 ?>
 </div>
